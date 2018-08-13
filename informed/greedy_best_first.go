140304991364e66443f0edd4902868f9d759b09c
Chris Statkiewicz <christopherstat@protonmail.com>
2018-08-13 20:15:12 +0300

    Greedy Best First Search

--- informed/greedy_best_first.go
+++ informed/greedy_best_first.go
@@ -6,19 +6,19 @@ import (
 	"time"
 )
 
-// GreedyBestFirst implements Greedy Best First Search.
+// AStar implements the A* algorithm.
 // Even though the function returns the shortest path between two vertices in a graph,
 // an optimal traversal between any two points can be built by using the return path map and func TraceSolutionPath.
 // If a path exists, it will be indicated by found and the traversal cost returned.
 func GreedyBestFirst(origin, target search.HeuristicState) (path map[search.State]search.State, found bool, cost float64) {
-	path, cumulativeCost, queue, open, closed := initBFSVariables()
+	path, cumulativeCost, queue, open, closed := initAStarVariables()
 
 	queue.Enqueue(origin, 0)
 	cumulativeCost[origin] = 0
 	for queue.Size() > 0 {
 		vertex := queue.Dequeue().(search.HeuristicState)
 		closed[vertex.Name()] = true
-		found = enqueueGBFSNeighbors(vertex, target, queue, open, closed, cumulativeCost, path)
+		found = enqueueBFNeighbors(vertex, target, queue, open, closed, cumulativeCost, path)
 		if found {
 			break
 		}
@@ -26,10 +26,10 @@ func GreedyBestFirst(origin, target search.HeuristicState) (path map[search.Stat
 	return path, found, cumulativeCost[target]
 }
 
-// Benchmark variant of GreedyBestFirst.
+// Benchmark variant of AStar.
 // It measures execution parameters (time, nodes expanded) them in a search.AlgorithmBenchmark entity.
 func BenchmarkGreedyBestFirst(origin, target search.HeuristicState) (path map[search.State]search.State, found bool, cost float64, bench search.AlgorithmBenchmark) {
-	path, cumulativeCost, queue, open, closed := initBFSVariables()
+	path, cumulativeCost, queue, open, closed := initAStarVariables()
 
 	start := time.Now()
 	var expansions uint = 0
@@ -40,7 +40,7 @@ func BenchmarkGreedyBestFirst(origin, target search.HeuristicState) (path map[se
 		vertex := queue.Dequeue().(search.HeuristicState)
 		closed[vertex.Name()] = true
 		expansions++
-		found = enqueueGBFSNeighbors(vertex, target, queue, open, closed, cumulativeCost, path)
+		found = enqueueBFNeighbors(vertex, target, queue, open, closed, cumulativeCost, path)
 		if found {
 			break
 		}
@@ -49,7 +49,7 @@ func BenchmarkGreedyBestFirst(origin, target search.HeuristicState) (path map[se
 	return path, found, cumulativeCost[target], search.AlgorithmBenchmark{ElapsedTime: elapsed, TotalExpansions: expansions}
 }
 
-func initBFSVariables() (path map[search.State]search.State, cumulativeCost map[search.State]float64, queue *gost.MinPriorityQueue, open, closed map[string]bool) {
+func initBFVariables() (path map[search.State]search.State, cumulativeCost map[search.State]float64, queue *gost.MinPriorityQueue, open, closed map[string]bool) {
 	path = make(map[search.State]search.State)
 	cumulativeCost = make(map[search.State]float64)
 	queue = new(gost.MinPriorityQueue) // Min as we need to obtain lowest costs first
@@ -58,31 +58,28 @@ func initBFSVariables() (path map[search.State]search.State, cumulativeCost map[
 	return
 }
 
-func enqueueGBFSNeighbors(vertex, target search.HeuristicState, queue *gost.MinPriorityQueue, open map[string]bool, closed map[string]bool, cumulativeCost map[search.State]float64, path map[search.State]search.State) (found bool) {
+func enqueueBFNeighbors(vertex, target search.HeuristicState, queue *gost.MinPriorityQueue, open map[string]bool, closed map[string]bool, cumulativeCost map[search.State]float64, path map[search.State]search.State) (found bool) {
 	if vertex.Equals(target) {
 		found = true
 		return
 	}
 	for _, neighbor := range vertex.Neighbors() {
-		cumulativeVertexCost := cumulativeCost[vertex]
-		adjacencyCost := vertex.Cost(neighbor)
-		cost := cumulativeVertexCost + adjacencyCost
-
 		_, visited := closed[neighbor.Name()]
-		prevCost, hasCost := cumulativeCost[neighbor]
-		if visited || hasCost && cost >= prevCost {
+		if visited {
 			continue
 		}
+
+		cumulativeVertexCost := cumulativeCost[vertex]
+		cost := cumulativeVertexCost + vertex.Cost(neighbor)
 		lowestCost, valueSet := cumulativeCost[neighbor]
 		if !valueSet || cost < lowestCost {
 			cumulativeCost[neighbor] = cost
 			path[neighbor] = vertex
-			_, queued := open[neighbor.Name()]
-			if !queued {
-				priority := cost + neighbor.(search.HeuristicState).Heuristic()
-				queue.Enqueue(neighbor, priority)
-				open[neighbor.Name()] = true
-			}
+			priority := neighbor.(search.HeuristicState).Heuristic()
+			// instead of a decrease-priority operation on existent nodes within the pq, we simply re-insert them with a different priority.
+			// See details at https://www.redblobgames.com/pathfinding/a-star/implementation.html#algorithm
+			queue.Enqueue(neighbor, priority)
+			open[neighbor.Name()] = true
 		}
 	}
 	return
--- test/informed/greedy_best_first_test.go
+++ test/informed/greedy_best_first_test.go
@@ -1,1 +1,65 @@
-package informed
+package informed_test
+
+import (
+	"testing"
+	"github.com/christat/dot"
+	"github.com/christat/search/informed"
+	tracer "github.com/christat/search"
+)
+
+func TestGreedyBestFirst(t *testing.T) {
+	ok, graph := dot.ParseFile("../test_dot_files/romania_manhattan_heuristic.dot", false)
+	graph.CostKey = "distance"
+	graph.HeuristicKey = "manhattan"
+	if !ok {
+		t.Errorf("Failed to parse AStar test file")
+	}
+	vertexMap := graph.VertexMap()
+	origin := vertexMap["Arad"]
+	target := vertexMap["Bucharest"]
+
+	path, found, cost := search.GreedyBestFirst(origin, target)
+	if !found {
+		t.Errorf("GreedyBestFirst failed to find valid path")
+	}
+	if cost != 450 {
+		t.Errorf("GreedyBestFirst cost computation is incorrect")
+	}
+
+	benchPath, found, cost, bench := search.BenchmarkGreedyBestFirst(origin, target)
+	if !found {
+		t.Errorf("Benchmark_GreedyBestFirst failed to find valid path")
+	}
+	if cost != 450 {
+		t.Errorf("Benchmark_GreedyBestFirst cost computation is incorrect")
+	}
+	if bench.TotalExpansions != 5 {
+		t.Errorf("Benchmark_GreedyBestFirst expansions calculation is incorrect")
+	}
+
+	res, _ := tracer.TraceSolutionPath(origin, target, path)
+	benchRes, _ := tracer.TraceSolutionPath(origin, target, benchPath)
+
+	expectedSolution := "Arad -> Sibiu -> Fagaras -> Bucharest"
+	foundSolution := res.String()
+	benchFoundSolution := benchRes.String()
+	if foundSolution != expectedSolution {
+		t.Errorf("Failed to find correct solution path in GreedyBestFirst.\nExpected: %v\nFound: %v", expectedSolution, foundSolution)
+	}
+	if benchFoundSolution != expectedSolution {
+		t.Errorf("Failed to find correct solution path in Benchmark_GreedyBestFirst.\nExpected: %v\nFound: %v", expectedSolution, benchFoundSolution)
+	}
+
+	origin = vertexMap["Dobreta"]
+	target = vertexMap["Fagaras"]
+	path, found, cost = search.GreedyBestFirst(origin, target)
+	res, _ = tracer.TraceSolutionPath(origin, target, path)
+	expectedSolution = "Dobreta -> Craiova -> RimnicuValcea -> Pitesti -> Bucharest -> Fagaras"
+	foundSolution = res.String()
+	if foundSolution != expectedSolution {
+		t.Errorf("Failed to find correct solution path in GreedyBestFirst.\nExpected: %v\nFound: %v", expectedSolution, foundSolution)
+	}
+	if cost != 677 {
+		t.Errorf("Benchmark_GreedyBestFirst cost computation is incorrect")
+	}
+}
