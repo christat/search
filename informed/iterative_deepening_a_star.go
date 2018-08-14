9151a79adfe2310ab0979e4c711038121f9e22cd
Chris Statkiewicz <christopherstat@protonmail.com>
2018-08-14 19:53:44 +0300

    IDA*

--- informed/iterative_deepening_a_star.go
+++ informed/iterative_deepening_a_star.go
@@ -1,1 +1,82 @@
 package search
+
+import (
+	"github.com/christat/search"
+	"math"
+)
+
+// IterativeDeepeningAStar implements the IDA* algorithm.
+// It is essentially a series of depth searches bounded by the minimum f value (cost + heuristic).
+// if no solution is found, the bound is updated with the minimum explored f value to continue the depth search.
+func IterativeDeepeningAStar(origin, target search.HeuristicState) (path map[search.State]search.State, found bool) {
+	path = make(map[search.State]search.State)
+
+	bound := origin.Heuristic()
+
+	for {
+		found, lowestTotalCost := heuristicBoundSearch(origin, target, 0, bound, path)
+		if found || lowestTotalCost == math.Inf(0) {
+			break
+		}
+		bound = lowestTotalCost
+	}
+	return path, found
+}
+
+func heuristicBoundSearch(vertex, target search.HeuristicState, branchCost, bound float64, path map[search.State]search.State) (found bool, newBound float64) {
+	f := branchCost + vertex.Heuristic()
+	if f > bound {
+		return false, f
+	}
+	if vertex.Equals(target) {
+		return true, bound
+	}
+	lowestTotalCost := math.Inf(0)
+	for _, neighbor := range vertex.Neighbors() {
+		path[neighbor] = vertex
+		found, lowestBound := heuristicBoundSearch(neighbor.(search.HeuristicState), target, branchCost + vertex.Cost(neighbor), bound, path)
+		if found {
+			break
+		}
+		if lowestBound < lowestTotalCost {
+			lowestTotalCost = lowestBound
+		}
+	}
+	return false, lowestTotalCost
+}
+
+// Benchmark variant of IterativeDeepeningAStar.
+// It measures execution parameters (time, nodes expanded) them in a search.AlgorithmBenchmark entity.
+/*func BenchmarkIterativeDeepeningAStar(origin, target search.State) (path map[search.State]search.State, found bool, bench search.AlgorithmBenchmark)  {
+	path = make(map[search.State]search.State)
+
+	start := time.Now()
+	var expansions uint = 0
+
+	for i := 0; i < maxDepth; i++ {
+		path, found, expansions = benchmarkDepthBoundSearch(origin, target, i, path, expansions)
+		if found {
+			break
+		}
+	}
+	elapsed := time.Since(start)
+	return path, found, search.AlgorithmBenchmark{ElapsedTime: elapsed, TotalExpansions: expansions}
+}
+
+func benchmarkHeuristicBoundSearch(vertex, target search.State, bound int, path map[search.State]search.State, expansions uint) (map[search.State]search.State, bool, uint) {
+	expansions++
+	var found bool
+	if vertex.Equals(target) {
+		return path, true, expansions
+	} else if bound > 0 {
+		for _, neighbor := range vertex.Neighbors() {
+			path[neighbor] = vertex
+			path, found, expansions = benchmarkDepthBoundSearch(neighbor, target, bound - 1, path, expansions)
+			if found {
+				break
+			}
+		}
+	}
+	return path, found, expansions
+}*/
+
--- test/informed/iterative_deepening_a_star_test.go
+++ test/informed/iterative_deepening_a_star_test.go
@@ -1,1 +1,46 @@
-package informed
+package informed_test
+
+import (
+	"testing"
+	"github.com/christat/dot"
+	tracer "github.com/christat/search"
+	"github.com/christat/search/informed"
+)
+
+func TestIterativeDeepeningAStar(t *testing.T) {
+	ok, graph := dot.ParseFile("../test_dot_files/heuristic_graph_test.dot", false)
+	graph.CostKey = "w"
+	graph.HeuristicKey = "h"
+	if !ok {
+		t.Errorf("Failed to parse IDA* test file")
+	}
+	vertexMap := graph.VertexMap()
+	origin := vertexMap["a"]
+	target := vertexMap["f"]
+
+	path, found := search.IterativeDeepeningAStar(origin, target)
+	if !found {
+		t.Errorf("IDA* failed to find valid path")
+	}
+	/*benchPath, found, bench := search.BenchmarkIterativeDeepening(origin, target, 10)
+	if !found {
+		t.Errorf("Benchmark_IDS failed to find valid path")
+	}
+	if bench.TotalExpansions != 7 {
+		t.Errorf("Benchmark_IDS expansions calculation is incorrect")
+	}*/
+
+	res, _ := tracer.TraceSolutionPath(origin, target, path)
+	//benchRes, _ := tracer.TraceSolutionPath(origin, target, benchPath)
+
+	expectedSolution := "a -> b -> e -> f"
+	foundSolution := res.String()
+	//benchFoundSolution := benchRes.String()
+	if foundSolution != expectedSolution {
+		t.Errorf("Failed to find correct solution path in IDA*.\nExpected: %v\nFound: %v", expectedSolution, foundSolution)
+	}
+	/*if benchFoundSolution != expectedSolution {
+		t.Errorf("Failed to find correct solution path in Benchmark_IDS.\nExpected: %v\nFound: %v", expectedSolution, benchFoundSolution)
+	}*/
+}
+
